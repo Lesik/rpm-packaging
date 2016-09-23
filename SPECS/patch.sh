7f466c0b3a353145e0adb4f557ffcf1cc10ce75c
Lesik <Lesik@users.noreply.github.com>
2016-09-23 19:51:48 +0200

    Move all changes for Telegram Desktop Patched to patch.sh

---
+++ SPECS/patch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ORIGSPEC="telegram-desktop.spec"
+PATCHEDSPEC="telegram-desktop-patched.spec"
+
+rm "$PATCHEDSPEC"
+cp "$ORIGSPEC" "$PATCHEDSPEC"
+
+sed -i 's/Name: telegram-desktop/Name: telegram-desktop-patched/g' $PATCHEDSPEC
+sed -i 's/a new era of messaging/a new, dark era of messaging/g' $PATCHEDSPEC
+
+sed -i 's/telegram.desktop/telegram-desktop-patched.desktop/g/' $PATCHEDSPEC
+
+sed -i '/Patch1: fix_cmake.patch/a\
+Patch2: about.diff\
+Patch3: messageradius_3px.diff\
+Patch4: maxmessagewidth_600px.diff\
+Patch5: emojipanel_1920_1080.diff' $PATCHEDSPEC
+
+sed -i '/patch -p1 -i %{PATCH1}/a\
+patch -p1 -i %{PATCH2}\
+patch -p1 -i %{PATCH3}\
+patch -p1 -i %{PATCH4}\
+patch -p1 -i %{PATCH5}' $PATCHEDSPEC
+
+sed -i /%changelog/Q $PATCHEDSPEC
